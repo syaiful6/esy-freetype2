8785b96bf266828415830732ba781e749bf3249f
Bryan Phelps <bryphe@outlook.com>
2018-11-13 21:13:51 -0800

    Fix compilation error when harfbuzz is in path (#7)
    * Try reproducing harfbuzz dep in build
    
    * Update configure scripts to ignore harfbuzz
    
    * Update pipeline
    
    * Fix name
    
    * Try adjusting casing (the example given in CMakeLists.txt uses camel case)

--- esy/configure-windows.sh
+++ esy/configure-windows.sh
@@ -5,4 +5,4 @@ INSTALL_PATH="$(cygpath -u $cur__install)"
 
 echo INSTALL: $INSTALL_PATH
 
-cmake -G "Unix Makefiles" ../ -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_LINKER=/usr/bin/x86_64-w64-mingw32-ld -DCMAKE_AR=/usr/bin/x86_64-w64-mingw32-ar -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH
+cmake -G "Unix Makefiles" ../ -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_LINKER=/usr/bin/x86_64-w64-mingw32-ld -DCMAKE_AR=/usr/bin/x86_64-w64-mingw32-ar -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DCMAKE_DISABLE_FIND_PACKAGE_HarfBuzz=TRUE
